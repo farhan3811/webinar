0516c471bf4ae393626b7f06e00835806a5fa4e0
Farhan <muhammadfarhan@staff.unsia.ac.id>
2024-11-13 00:59:11 +0700

    Ganti logo

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                        <img src="{{ url('/gambar/logobiru.png') }}" width="100" alt="">
                     </a>
                 </div>
 
@@ -18,7 +18,7 @@
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
     <x-nav-link :href="route('datamahasiswa')" :active="request()->routeIs('datamahasiswa')">
-        {{ __('DataMahasiswa') }}
+        {{ __('Data Mahasiswa') }}
     </x-nav-link>
 </div>
 
