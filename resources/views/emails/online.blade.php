c8b1435df749360e24b34e492b2634196ac5d81d
Farhan <muhammadfarhan@staff.unsia.ac.id>
2024-12-06 14:18:53 +0700

    Revisi

--- resources/views/emails/online.blade.php
+++ resources/views/emails/online.blade.php
@@ -6,27 +6,27 @@
     <title>Undangan Wisuda</title>
 </head>
 <body style="font-family: Arial, sans-serif; line-height: 1.6; margin: 20px; color: #333;">
-    <p><a href="https://bit.ly/3Zp8KTX">Undangan Wisuda Online</a></p>
+    <p>Undangan Wisuda Online<a href="https://bit.ly/3Zp8KTX">Link</a></p>
     <br>
-    <p>Kepada Yth.<br>
-    <strong>{{ $registration->name }}</strong><br>
+    <p><strong>Kepada Yth.</strong><br>
+    {{ $registration->name }}<br>
     Program Studi {{ $registration->program_studi }}<br>
     di Tempat</p>
-
+<br>
     <p>Dengan hormat,</p>
-
+<br>
     <p>Dengan penuh rasa syukur dan kebanggaan, kami mengundang Saudara/i untuk menghadiri acara Wisuda Program Sarjana (S1) Universitas Siber Asia. Acara ini merupakan puncak dari perjalanan akademik Saudara/i dan tanda penghargaan atas usaha dan dedikasi yang telah Saudara/i tunjukkan selama menempuh pendidikan di Universitas Siber Asia. Adapun acara wisuda akan dilaksanakan pada:</p>
 
-    <p><strong>Hari, Tanggal:</strong> Minggu, 08 Desember 2024<br>
-    <strong>Waktu:</strong> 08:00 s.d 12:30 WIB<br>
+    <p>Hari, Tanggal: Minggu, 08 Desember 2024<br>
+    Waktu: 08:00 s.d 12:30 WIB<br>
     <strong>Tempat:</strong> ONLINE (ZOOM)<br>
-    <strong>Link Zoom:</strong> <a href="https://bit.ly/WisudaUnsia2" target="_blank">https://bit.ly/WisudaUnsia2</a><br>
-    <strong>Meeting ID:</strong> 975 0128 8579</p>
-    <p><strong>Virtual Background Zoom:</strong> <a href="https://bit.ly/BGZoomWisudaUnsia2" target="_blank">https://bit.ly/BGZoomWisudaUnsia2</a><br>
-    <strong>Live Streaming YouTube:</strong> <a href="https://youtube.com/live/dr01Tjfc14Q?feature=share" target="_blank">https://youtube.com/live/dr01Tjfc14Q?feature=share</a></p>
-
+    Link Zoom: <a href="https://bit.ly/WisudaUnsia2" target="_blank">https://bit.ly/WisudaUnsia2</a><br>
+    Meeting ID: 975 0128 8579</p>
+    <p>Virtual Background Zoom: <a href="https://bit.ly/BGZoomWisudaUnsia2" target="_blank">https://bit.ly/BGZoomWisudaUnsia2</a><br>
+    Live Streaming YouTube: <a href="https://youtube.com/live/dr01Tjfc14Q?feature=share" target="_blank">https://youtube.com/live/dr01Tjfc14Q?feature=share</a></p>
+<br>
     <p>Kami berharap kehadiran Saudara/i dapat memeriahkan acara ini dan menjadikannya momen berharga dalam hidup Saudara/i.</p>
-
+<br>
     <p>Demikian undangan ini kami sampaikan. Atas perhatian dan kehadiran Anda, kami ucapkan terima kasih.</p>
    <br>
     <p>Link Scan QR Code Absensi Wisuda: <a href="https://bit.ly/3VmvEui">https://bit.ly/3VmvEui</a></p>
@@ -34,15 +34,15 @@
     <p><strong>Hormat kami,</strong><br>
     Tim Pendaftaran Wisuda<br>
     Universitas Siber Asia</p>
-
+<br>
     <p><strong>Kontak Bagian Pendaftaran:</strong><br>
     Fian: (+62) 815-1469-6934<br>
     Holis: (+62) 812-1898-7353</p>
 
-    <p><stong>Silakan simak informasi terkait wisuda online berikut ini. Sampai bertemu pada acara Wisuda Universitas Siber Asia Periode 2</stong></p>
+    <p><strong>Silakan simak informasi terkait wisuda online berikut ini. Sampai bertemu pada acara Wisuda Universitas Siber Asia Periode 2</strong></p>
 <br>
-    <p><a href="https://bit.ly/3ZDSEr2" target="_blank">Tata Tertib Wisudawan/Wati secara Online</a><br>
-    <a href="https://bit.ly/3OJ9T41" target="_blank">Petunjuk Masuk Acara Wisuda Khusus Wisudawan/Wati secara Online</a><br>
-    <a href="https://bit.ly/41llBcy" target="_blank">FAQ Wisuda UNSIA</a></p>
+   <p>Tata Tertib Wisudawan/Wati secara Online<a href="https://bit.ly/3ZDSEr2" target="_blank">Link</a></p> <br>
+   <p>Petunjuk Masuk Acara Wisuda Khusus Wisudawan/Wati secara Online</p> <a href="https://bit.ly/3OJ9T41" target="_blank">Link</a><br>
+    <p>FAQ Wisuda UNSIA<a href="https://bit.ly/41llBcy" target="_blank">Link</a></p> 
 </body>
 </html>
--- resources/views/emails/onsite.blade.php
+++ resources/views/emails/onsite.blade.php
@@ -8,42 +8,43 @@
 </head>
 
 <body>
-    <p><a href="https://bit.ly/3Zl8OUN">Undangan Wisuda</a></p>
-    <p><a href="https://bit.ly/3CY0L8Z">Denah Pendaftaran Wisuda</a></p>
+    <p>Undangan Wisuda<a href="https://bit.ly/3Zl8OUN">Link</a></p>
+    <p>Denah Pendaftaran Wisuda<a href="https://bit.ly/3CY0L8Z">Link</a></p>
 
     <p><strong>Kepada Yth.</strong><br>
         {{ $registration->name }}<br>
         Program Studi {{ $registration->program_studi }}<br>
         di Tempat</p>
+        <br>
 
     <p>Dengan hormat,</p>
-
+<br>
     <p>Dengan penuh rasa syukur dan kebanggaan, kami mengundang Saudara/i untuk menghadiri acara Wisuda Program Sarjana
         (S1) Universitas Siber Asia. Acara ini merupakan puncak dari perjalanan akademik Saudara/i dan tanda penghargaan
         atas usaha dan dedikasi yang telah Saudara/i tunjukkan selama menempuh pendidikan di Universitas Siber Asia.
         Adapun acara wisuda akan dilaksanakan pada:</p>
-
-    <p><strong>Hari, Tanggal:</strong> Minggu, 08 Desember 2024<br>
-        <strong>Waktu:</strong> 08:00 s.d 12:30 WIB<br>
-        <strong>Tempat:</strong> <em>UNAS AUDITORIUM</em><br>
-        <strong>Alamat:</strong> Jl. Sawo Manila No.61, Pejaten, Pasar Minggu, Jakarta Selatan
+<br>
+    <p>Hari, Tanggal: Minggu, 08 Desember 2024<br>
+        Waktu: 08:00 s.d 12:30 WIB<br>
+        Tempat: <strong>UNAS AUDITORIUM</strong><br>
+        Alamat: Jl. Sawo Manila No.61, Pejaten, Pasar Minggu, Jakarta Selatan
     </p>
 
     <p><img src="{{ $message->embed($barcodePath) }}" alt="QR Code"></p>
 
     <p>Kami berharap kehadiran Saudara/i beserta keluarga dapat memeriahkan acara ini dan menjadikannya momen berharga
-        dalam hidup Saudara/i.</p>
+        dalam hidup Saudara/i.</p><br>
 
     <p>Demikian undangan ini kami sampaikan. Atas perhatian dan kehadiran Anda, kami ucapkan terima kasih.</p>
-
+<br>
     <p><strong>Hormat kami,</strong><br>
         Tim Pendaftaran Wisuda<br>
         Universitas Siber Asia</p>
-
+<br>
     <p><strong>Kontak Bagian Pendaftaran:</strong><br>
         Fian: (+62) 815-1469-6934<br>
         Holis: (+62) 812-1898-7353</p>
-
+<br>
     <p><strong>Silakan simak informasi terkait wisuda onsite berikut ini. Sampai bertemu pada acara Wisuda Universitas
             Siber Asia Periode 2.</strong></p>
     <br>
@@ -51,23 +52,22 @@
         Kode Unik untuk Keluarga Pendamping adalah<strong>{{ $registration->kode_unik }}</strong><br>
         Mohon pastikan kode unik ini diinformasikan kepada keluarga pendamping untuk kelancaran proses pendaftaran.
     </p>
-
-    <p><a href="https://bit.ly/3ZDSEr2">Tata Tertib Wisudawan/Wati secara Onsite</a><br>
-        <a href="https://bit.ly/3OJ9T41">Petunjuk Masuk Acara Wisuda Khusus Wisudawan/Wati secara Onsite</a><br>
-        <a href="https://bit.ly/3CY0L8Z">Denah Wisuda</a><br>
-        <a href="https://bit.ly/3OJ9T41">Petunjuk Masuk Acara Wisuda Khusus Keluarga Pendamping & Tambahan</a><br>
-        <a href="https://bit.ly/49s3HqK">Rekomendasi Akomodasi Penginapan</a>
-    </p>
-
+<br>
+<p>Tata Tertib Wisudawan/Wati secara Onsite<a href="https://bit.ly/3ZDSEr2">Link</a></p><br>
+        <p>Petunjuk Masuk Acara Wisuda Khusus Wisudawan/Wati secara Onsite<a href="https://bit.ly/3OJ9T41">Link</a></p><br>
+        <p>Denah Wisuda<a href="https://bit.ly/3CY0L8Z">Link</a></p><br>
+        <p>Petunjuk Masuk Acara Wisuda Khusus Keluarga Pendamping & Tambahan<a href="https://bit.ly/3OJ9T41">Link</a></p><br>
+        <p>Rekomendasi Akomodasi Penginapan<a href="https://bit.ly/49s3HqK">Link</a></p>
+<br>
     <p><strong>Imbauan:</strong><br>
-        - Selama acara wisuda berlangsung, Calon Wisudawan/Wati tidak diperkenankan makan hingga acara selesai. Kami
+        1. Selama acara wisuda berlangsung, Calon Wisudawan/Wati tidak diperkenankan makan hingga acara selesai. Kami
         menyarankan agar Calon Wisudawan/Wati sarapan terlebih dahulu di pagi hari. Untuk snack dapat dititipkan kepada
         keluarga yang mendampingi.<br>
-        - Bagi Keluarga Pendamping & Tambahan selama acara wisuda berlangsung, dimohon untuk menjaga ketertiban dan
+        2. Bagi Keluarga Pendamping & Tambahan selama acara wisuda berlangsung, dimohon untuk menjaga ketertiban dan
         ketenangan hingga acara selesai.<br>
-        - Harap membuang sampah pada tempat yang telah disediakan untuk menjaga kebersihan lokasi acara Wisuda.</p>
+        3. Harap membuang sampah pada tempat yang telah disediakan untuk menjaga kebersihan lokasi acara Wisuda.</p>
 
-    <p><a href="https://bit.ly/41llBcy">FAQ Wisuda UNSIA</a></p>
+    <p>FAQ Wisuda UNSIA<a href="https://bit.ly/41llBcy">Link</a></p>
 </body>
 
 </html>
